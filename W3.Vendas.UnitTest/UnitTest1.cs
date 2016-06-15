81095c61dce8af96adf760fa7454b724379ec8b1
marciodouglas <marciodouglas@gmail.com>
2016-06-15 17:48:39 -0300

    Initial Commit

---
+++ W3.Vendas.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace W3.Vendas.UnitTest
+{
+    [TestClass]
+    public class UnitTestW3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ W3.Vendas.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="W3.Vendas.Web.MvcApplication" Language="C#" %>
